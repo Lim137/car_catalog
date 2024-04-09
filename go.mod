1c8211384fd4cabd309332fbadc7c3af398ae547
Lim137 <sergei.ignatov2001@inbox.ru>
2024-04-09 21:21:34 +0300

    server without routes

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/Lim137/car_catalog
 go 1.21.6
 
 require (
-	github.com/go-chi/cors v1.2.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
 )
--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	godotenv.Load(".env")
 	portString := os.Getenv("PORT")
-
-	fmt.Println(portString)
+	router := chi.NewRouter()
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+	log.Println("Server is running on port " + portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
