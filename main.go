769d64d119d2473412242c4d67e1c009662484e7
Lim137 <sergei.ignatov2001@inbox.ru>
2024-04-09 21:23:36 +0300

    added CORS

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
@@ -12,6 +13,15 @@ func main() {
 	godotenv.Load(".env")
 	portString := os.Getenv("PORT")
 	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
